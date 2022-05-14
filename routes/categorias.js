ebb03a02a8e66508dfda900df2ade7f6dc34854c
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-14 18:30:43 -0300

    feat: Implementa novo metodo

--- routes/categorias.js
+++ routes/categorias.js
@@ -8,4 +8,9 @@ router.get("/", async (req, res) => {
   res.status(200).send(rows);
 });
 
+router.post("/", async (req, res) => {
+  const query = "INSERT INTO categorias (nm_categoria) VALUES ($1)";
+  const { rows } = await db.query(query, [req.body.nm_categoria]);
+  res.status(200).send(rows);
+});
 module.exports = router;

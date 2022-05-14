0421c1b8144f149635ea0ba1e9c88f080a338c03
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-14 17:49:32 -0300

    feat: Cria funcao geral

--- db/index.js
+++ db/index.js
@@ -2,3 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool();
 
+module.exports = {
+  query: (text, params) => {
+    console.log("Query executed - " + (new Date()) );
+    return pool.query(text, params)
+  }
+}

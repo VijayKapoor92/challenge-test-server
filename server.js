afc4fd187e576fdf5e1c550b42e7d910a265a902
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-16 00:57:38 -0300

    refactor: Implementa arquivo de setup

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
-
 const app = express();
-const port = 3001;
+const main = require("./main");
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-
-const routes = require("./routes")
-routes(app);
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-});
+main(app);

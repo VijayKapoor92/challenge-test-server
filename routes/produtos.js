79f712dfbd48475bb99c52766773a1a35f09aa31
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-16 00:58:46 -0300

    feat: Implementa import

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,4 +1,7 @@
 const { Router } = require("express");
+const fs = require("fs");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const db = require("../db");
 
 const router = new Router();
@@ -73,4 +76,59 @@ router.delete("/:id", async (req, res) => {
   res.status(200).send(rows);
 });
 
+router.post("/import", upload.single("file"), async (req, res) => {
+  const { id_categoria } = req.body;
+  const file = req.file;
+
+  let data = JSON.parse(fs.readFileSync(file.path));
+  let payload = null;
+  if (!data.length)
+    res.send({
+      status: -1,
+      message: "Arquivo sem dados.",
+      payload: []
+    });
+  
+  let response_array = [];
+  let i = 0;
+  while(i < data.length) {
+    const produto = data[i];
+    try {
+      const query = `
+      INSERT INTO produtos 
+      (nm_produto, qt_produto, vl_produto, id_categoria) 
+      (SELECT $1 AS nm_produto, 
+              $2 AS qt_produto, 
+              $3 AS vl_produto, 
+              (SELECT id_categoria from categorias WHERE LOWER(nm_categoria) = LOWER($4)) AS id_categoria
+      )
+
+      RETURNING *
+      `;
+
+      let vl_produto = produto.vl_produto;
+      if (vl_produto.includes(","))
+        vl_produto = vl_produto.replace(",", ".");
+
+      const params = [
+        produto.nm_produto,
+        produto.qt_produto,
+        vl_produto,
+        produto.nm_categoria
+      ]
+
+      const { rows } = await db.query(query, params);
+      response_array.push(rows[0]);
+      i++;
+    } catch(err) {
+      console.error(err);
+    }
+  }
+  
+  res.status(201).send({
+    status: 1,
+    message: "Produtos importados com sucesso!",
+    payload: response_array
+  });
+});
 module.exports = router;

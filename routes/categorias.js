d3ff1476f053caed7980fbfd81c3f6a77392162a
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-17 14:34:40 -0300

    feat: Implementa queries

--- routes/categorias.js
+++ routes/categorias.js
@@ -1,36 +1,44 @@
 const Router = require("express-promise-router");
 const db = require("../db");
 
+const queries = require("../queries");
 const router = new Router();
 
 module.exports = router;
 
 router.get("/", async (_, res) => {
-  const query = `
-  SELECT cat.id_categoria,
-         cat.nm_categoria,
-         (select count(*) from produtos where id_categoria = cat.id_categoria) as tl_produtos
-    FROM categorias cat
-  `;
-  const { rows } = await db.query(query);
+  const { rows } = await db.query(queries.categorias.getAll);
   res.status(200).send(rows);
 });
 
 router.post("/", async (req, res) => {
-  const query = "INSERT INTO categorias (nm_categoria) VALUES ($1) RETURNING *";
-  const { rows } = await db.query(query, [req.body.nm_categoria]);
+  const { rows } = await db.query(queries.categorias.insert, [req.body.nm_categoria]);
   res.status(201).send(rows);
 });
 
 router.put("/:id", async (req, res) => {
   const id_categoria = req.params.id;
-  const query = "UPDATE categorias SET nm_categoria = $1 WHERE id_categoria = $2 RETURNING *";
-  const { rows } = await db.query(query, [req.body.nm_categoria, id_categoria]);
+  const nm_categoria = req.body.nm_categoria;
+
+  const { rows } = await db.query(queries.categorias.update, [nm_categoria, id_categoria]);
   res.status(200).send(rows[0]);
 });
 
 router.delete("/:id", async (req, res) => {
   const id_categoria = req.params.id;
-  const { rows } = await db.query("DELETE FROM categorias WHERE id_categoria = $1", [id_categoria]);
+  const { rows } = await db.query(queries.categorias.delete, [id_categoria]);
   res.status(200).send(rows);
+});
+
+router.post("/:id/export", async (req, res) => {
+  try {
+    const { rows } = await db.query(queries.categorias.export, [req.params.id]);
+    res.status(200).send({
+      status: 1,
+      message: "",
+      payload: rows
+    });
+  } catch(err) {
+    console.error(err);
+  }
 });
--- routes/produtos.js
+++ routes/produtos.js
@@ -4,35 +4,18 @@ const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const db = require("../db");
 
+const queries = require("../queries");
+
 const router = new Router();
 
-router.get("/", async (req, res) => {
-  const query = `
-  SELECT pro.id_produto,
-         pro.nm_produto,
-         pro.qt_produto,
-         pro.vl_produto,
-         pro.id_categoria,
-         cat.nm_categoria
-    FROM produtos pro,
-         categorias cat
-   WHERE pro.id_categoria = cat.id_categoria
-  `;
-  const { rows } = await db.query(query);
+router.get("/", async (_, res) => {
+  const { rows } = await db.query(queries.produtos.getAll);
   res.status(200).send(rows);
 });
 
 router.post("/", async (req, res) => {
-  const query = `
-  INSERT INTO produtos 
-  (nm_produto, qt_produto, vl_produto, id_categoria) 
-  VALUES 
-  ($1, $2, $3, $4)
-
-  RETURNING *
-  `;
-
   let vl_produto = req.body.vl_produto;
+  
   if (vl_produto.includes(","))
     vl_produto = vl_produto.replace(",", ".");
 
@@ -43,21 +26,13 @@ router.post("/", async (req, res) => {
     req.body.id_categoria
   ]
 
-  const { rows } = await db.query(query, params);
+  const { rows } = await db.query(queries.produtos.insert, params);
   res.status(201).send(rows[0]);
 });
 
 router.put("/:id", async (req, res) => {
   const id_produto = req.params.id;
-  const query = `
-  UPDATE produtos 
-     SET nm_produto = $1,
-         qt_produto = $2,
-         vl_produto = $3,
-         id_categoria = $4
-   WHERE id_produto = $5
-  RETURNING *
-  `;
+  
   const params = [
     req.body.nm_produto,
     req.body.qt_produto,
@@ -66,13 +41,13 @@ router.put("/:id", async (req, res) => {
     id_produto
   ];
   
-  const { rows } = await db.query(query, params);
+  const { rows } = await db.query(queries.produtos.update, params);
   res.status(200).send(rows);
 });
 
 router.delete("/:id", async (req, res) => {
   const id_produto = req.params.id;
-  const { rows } = await db.query("DELETE FROM produtos WHERE id_produto = $1", [id_produto]);
+  const { rows } = await db.query(queries.produtos.delete, [id_produto]);
   res.status(200).send(rows[0]);
 });
 
@@ -87,23 +62,22 @@ router.post("/import", upload.single("file"), async (req, res) => {
       payload: []
     });
   
+  let categoria = null;
+  let query = "";
   let payload = [];
   let i = 0;
+
   while(i < data.length) {
     const produto = data[i];
+    if (("categoria" in produto) && produto.nm_categoria.length) {
+      categoria = produto.nm_categoria;
+      query = queries.produtos.importByName;
+    } else {
+      categoria = req.body.id_categoria;
+      query = queries.produtos.importById;
+    }
+
     try {
-      const query = `
-      INSERT INTO produtos 
-      (nm_produto, qt_produto, vl_produto, id_categoria) 
-      (SELECT $1 AS nm_produto, 
-              $2 AS qt_produto, 
-              $3 AS vl_produto, 
-              (SELECT id_categoria from categorias WHERE LOWER(nm_categoria) = LOWER($4)) AS id_categoria
-      )
-
-      RETURNING *
-      `;
-
       let vl_produto = produto.vl_produto;
       if (vl_produto.includes(","))
         vl_produto = vl_produto.replace(",", ".");
@@ -112,15 +86,20 @@ router.post("/import", upload.single("file"), async (req, res) => {
         produto.nm_produto,
         produto.qt_produto,
         vl_produto,
-        produto.nm_categoria
-      ]
+        categoria
+      ];
 
       const { rows } = await db.query(query, params);
       payload.push(rows[0]);
-      i++;
     } catch(err) {
       console.error(err);
+      res.status(200).send({
+        status: -1,
+        message: "Error"
+      });
     }
+
+    i++;
   }
   
   res.status(201).send({
@@ -128,31 +107,6 @@ router.post("/import", upload.single("file"), async (req, res) => {
     message: "Produtos importados com sucesso!",
     payload
   });
-});
-
-router.post("/:id/export", async (req, res) => {
-  try {
-    const query = `
-    SELECT pro.id_produto,
-          pro.nm_produto,
-          pro.qt_produto,
-          pro.vl_produto,
-          pro.id_categoria,
-          cat.nm_categoria
-      FROM produtos pro,
-          categorias cat
-    WHERE pro.id_categoria = cat.id_categoria 
-      AND cat.id_categoria = $1
-    `;
-    const { rows } = await db.query(query, [req.params.id]);
-    res.status(200).send({
-      status: 1,
-      message: "",
-      payload: rows
-    });
-  } catch(err) {
-    console.error(err);
-  }
 });
 
 module.exports = router;

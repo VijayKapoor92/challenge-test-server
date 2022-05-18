a6590ce7b6c0a45332584019024b1ca53ed633b8
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-18 02:02:00 -0300

    fix: Arruma bugs

--- routes/produtos.js
+++ routes/produtos.js
@@ -60,44 +60,37 @@ router.post("/import", upload.single("file"), async (req, res) => {
 
   let data = JSON.parse(fs.readFileSync(file.path));
   if (!data.length)
-    res.send({
+    return res.send({
       status: -1,
       message: "Arquivo sem dados.",
       payload: []
     });
   
-  let categoria = null;
-  let query = "";
   let payload = [];
   let i = 0;
 
   while(i < data.length) {
-    const produto = data[i];
-    if (("categoria" in produto) && produto.nm_categoria.length) {
-      categoria = produto.nm_categoria;
-      query = queries.produtos.importByName;
-    } else {
-      categoria = req.body.id_categoria;
-      query = queries.produtos.importById;
-    }
-
     try {
+      const produto = data[i];
+    
+      const id_categoria = req.body.id_categoria;
+
       let vl_produto = produto.vl_produto;
       if (vl_produto.includes(","))
         vl_produto = vl_produto.replace(",", ".");
 
       const params = [
         produto.nm_produto,
-        produto.qt_produto,
-        vl_produto,
-        categoria
+        parseInt(produto.qt_produto, 10),
+        parseFloat(vl_produto),
+        id_categoria
       ];
 
-      const { rows } = await db.query(query, params);
+      const { rows } = await db.query(queries.produtos.import, params);
       payload.push(rows[0]);
     } catch(err) {
       console.error(err);
-      res.status(200).send({
+      return res.status(200).send({
         status: -1,
         message: "Error"
       });

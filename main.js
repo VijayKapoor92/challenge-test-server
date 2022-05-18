4ffd4d383560fab0e89a89827a298d4fd4b5c427
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-18 14:01:45 -0300

    refactor: Remove codigo nao utilizado

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-// const fileUpload = require("express-fileupload");
 const bodyParser = require("body-parser");
 const routes = require("./routes")
 const port = 3001;
 
 module.exports = app => {
-  // app.use(fileUpload());
   app.use(bodyParser.json());
   app.use(
     bodyParser.urlencoded({
